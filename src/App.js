0a9d0c3896f9350e3f9e604d0145015dbf890113
Dane <danehague1@googlemail.com>
2024-02-11 21:19:06 +0000

    Several Updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { HashRouter as Router, Route, Switch, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Routes, Navigate } from 'react-router-dom';
 import HomePage from './features/home/HomePage';
 import CurrencyPage from './features/currency/CurrencyPage';
 import NavBar from './features/navbar/NavBar';
@@ -13,6 +13,7 @@ function App() {
     <Router>
       <NavBar />
       <Routes>
+      <Route path="/" element={<Navigate replace to="/Home" />} />
         <Route path="/Home" element={<HomePage />} />
         <Route path="/Currency" element={<CurrencyPage />} />
         <Route path="/Essence" element={<EssencePage />} />
--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -1,11 +1,46 @@
 import React from "react";
+import './HomePage.css';
 
 const HomePage = () => {
     return (
-        <>
-        Simple react app to display Path of Exile data
-        </>
-    )
-}
+        <div className="homePage">
+            <header className="header">
+                <h1>PoE Currency Tracker</h1>
+            </header>
+            <main className="content">
+                <p>Small application to track my currency in <a href="https://www.pathofexile.com/" target="_blank">Path of Exile</a>, a game created by Grinding Gear Games</p>
+                <br />
+                <p>How it works!</p>
+                <p>I have a function app hosted in Azure that pulls data from Path of Exile's API every hour.</p>
+                <p>I use Timer Triggers to pull the data on the hour and HttpTriggers to query the Redis cache.</p>
+                <p>It then stores the data in Cosmos and in Redis, Redis is hosted inside a K8 cluster in Azure.    </p>
+                <br />
+                <p>I use a myriad of technologies</p>
+                <ul>
+                    <li>
+                        C#
+                    </li>
+                    <li>
+                        React
+                    </li>
+                    <li>
+                        Azure Function App (Timer Triggers, HTTP Triggers) 
+                    </li>
+                    <li>
+                        K8
+                    </li>
+                    <li>
+                        Redis
+                    </li>
+                    <li>
+                        Cosmos DB
+                    </li>
+                </ul>
+            </main>
+            <footer className="footer">
+            </footer>
+        </div>
+    );
+};
 
 export default HomePage;
